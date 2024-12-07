dabf0f4f74055b483545ea5e7bbad7ceee332227
Lennart Querter <lennart.querter@gmail.com>
2024-12-07 18:14:35 +0100

    Remove ignored files

--- modules/sequencers/2-digital/stm_sequencer_411-RE/cmake-build-debug/CMakeFiles/stm_sequencer_411-RE.elf.dir/Core/Src/app/main_app.c.obj.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/stm_sequencer_411-RE.elf.dir/Core/Src/app/main_app.c.obj: \
- /Users/lennart.querter/go/src/lenimal.nl/euro-rack-synth/modules/sequencers/2-digital/stm_sequencer_411-RE/Core/Src/app/main_app.c
--- modules/sequencers/2-digital/stm_sequencer_411-RE/cmake-build-debug/CMakeFiles/stm_sequencer_411-RE.elf.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty compiler generated dependencies file for stm_sequencer_411-RE.elf.
-# This may be replaced when dependencies are built.
